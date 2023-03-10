a1054add092a84eb6e046dedc0c99fb5d4f56ecf
thirumoorthy2005 <thirumoorthy@capeindia.net>
2023-03-10 18:14:52 +0530

    lps service updated

--- src/main/java/com/capeelectric/LpsInspectionApplication.java
+++ src/main/java/com/capeelectric/LpsInspectionApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
  
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableEurekaClient
 public class LpsInspectionApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
